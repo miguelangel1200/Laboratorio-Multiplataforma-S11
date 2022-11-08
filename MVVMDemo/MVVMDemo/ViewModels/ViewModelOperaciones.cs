53c0f7533e8a20f7c7d6dfff4815f12c472d1703
htorricotecsup <53871912+htorrico@users.noreply.github.com>
2022-11-08 11:43:31 -0500

    Add project files.

---
+++ MVVMDemo/MVVMDemo/ViewModels/ViewModelOperaciones.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MVVMDemo.ViewModels
+{
+    public class ViewModelOperaciones :ViewModelBase
+    {
+
+        #region  Atributos
+        int valorA;
+        public int ValorA
+        {
+            get { return valorA; }
+            set
+            {
+                if (valorA != value)
+                {
+                    valorA = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        int valorB;
+        public int ValorB
+        {
+            get { return valorB; }
+            set
+            {
+                if (valorB != value)
+                {
+                    valorB = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        int resultadoSuma;
+        public int ResultadoSuma
+        {
+            get { return resultadoSuma; }
+            set
+            {
+                if (resultadoSuma != value)
+                {
+                    resultadoSuma = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        #endregion
+
+        public ICommand Sumar { protected set; get; }
+
+
+        public ViewModelOperaciones()
+        {
+            Sumar = new Command(() =>
+            {
+                ResultadoSuma = ValorA + ValorB;
+            });
+
+        }       
+    }
+}
